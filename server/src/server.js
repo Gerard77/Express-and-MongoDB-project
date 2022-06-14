9a85efee78b119c7a0fe8a93df03fa571f0a55c6
Gerard <gery261@gmail.com>
2022-06-14 20:27:56 +0200

    Added middleware and tested first endpoint. Updated gitignore

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,17 @@
-console.log("aefeafeaf");
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+app.disable('x-powered-by');
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get("/",async (req, res)=>{
+    res.status(200).send({ send: "heilow!"});
+});
+
+app.listen(8082,()=>{
+    console.log("Forums API listening on: 8082");
+});
